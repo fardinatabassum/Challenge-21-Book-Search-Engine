668668baf39302dab449a5c3599b20ecbb2122c2
fardinatabassum <fardinatabassum@gmail.com>
2023-05-31 00:28:50 -0400

    added readme

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,30 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route, BrowserRouter, Routes } from "react-router-dom";
 import SearchBooks from "./pages/SearchBooks";
 import SavedBooks from "./pages/SavedBooks";
 import Navbar from "./components/Navbar";
 import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "/graphql",
-    cache: new InMemoryCache(),
+  uri: "/graphql",
+  cache: new InMemoryCache(),
 });
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <Router>
-                <>
-                    <Navbar />
-                    <Switch>
-                        <Route exact path="/" component={SearchBooks} />
-                        <Route exact path="/saved" component={SavedBooks} />
-                        <Route
-                            render={() => (
-                                <h1 className="display-2">Wrong page!</h1>
-                            )}
-                        />
-                    </Switch>
-                </>
-            </Router>
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <BrowserRouter>
+        <>
+          <Navbar />
+          <Routes>
+            <Route exact path="/" element={SearchBooks} />
+            <Route exact path="/saved" element={SavedBooks} />
+            <Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+          </Routes>
+        </>
+      </BrowserRouter>
+    </ApolloProvider>
+  );
 }
 
 export default App;
